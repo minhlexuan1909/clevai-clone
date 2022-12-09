cfcd81bf315dd0747782fb68c7955579f560b69e
minhlexuan1909 <minhlx1909@gmail.com>
2022-12-09 16:05:05 +0700

    [FIX] Checkbox not display

--- src/modules/common/components/Checkbox/Checkbox.tsx
+++ src/modules/common/components/Checkbox/Checkbox.tsx
@@ -10,7 +10,7 @@ const Checkbox: React.FC<Props> = (props) => {
   const { isSelected, onClick } = props;
   return (
     <div
-      className={`checkbox${isSelected && " checkbox--checked"}`}
+      className={`checkbox${isSelected ? " checkbox--checked" : ""}`}
       onClick={onClick}
     ></div>
   );

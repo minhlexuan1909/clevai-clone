94ac1e51cb9b1b63225522b33466af6d677562e7
minhlexuan1909 <minhlx1909@gmail.com>
2022-12-09 12:57:52 +0700

    [UPDATE] Quiz best answer container

---
+++ src/modules/quiz/components/QuizBestAnswerWrapper/QuizBestAnswerWrapper.tsx
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import ButtonContinueWrapper from "../ButtonContinue/ButtonContinueWrapper";
+import QuizBestAnswerOptionWrapper from "../QuizBestAnswerOptionWrapper/QuizBestAnswerOptionWrapper";
+import "./QuizBestAnswerWrapper.scss";
+
+const QuizBestAnswerWrapper = () => {
+  const [isAnswered, setIsAnswered] = useState<boolean>(false);
+  const [selectedOption, setSelectedOption] = useState<null | number>(null);
+  const isOptionSelected = selectedOption !== null;
+
+  const [isSelectedCorrect, setIsSelectedCorrect] = useState<boolean>(false);
+  const quizOptions = [
+    { id: 1, description: "1203g", isCorrect: true },
+    { id: 2, description: "1204g", isCorrect: false },
+    { id: 3, description: "1205g", isCorrect: false },
+    { id: 4, description: "1206g", isCorrect: false },
+  ];
+
+  const handleSelectOption = (
+    idQuizOption: number,
+    isQuizOptionCorrect: boolean
+  ) => {
+    if (!isAnswered) {
+      setSelectedOption(idQuizOption);
+      setIsSelectedCorrect(isQuizOptionCorrect);
+    }
+  };
+
+  const handleButtonContinueClick = () => {
+    setIsAnswered(true);
+  };
+  return (
+    <div className="quiz-best-answer quiz-container">
+      {quizOptions.map((quiz) => (
+        <QuizBestAnswerOptionWrapper
+          key={quiz.id}
+          isTempSelected={false}
+          isSelected={quiz.id === selectedOption}
+          isAnswered={isAnswered}
+          isCorrect={quiz.isCorrect}
+          onClick={() => handleSelectOption(quiz.id, quiz.isCorrect)}
+        >
+          {quiz.description}
+        </QuizBestAnswerOptionWrapper>
+      ))}
+      <ButtonContinueWrapper
+        isCompleteAnswer={isOptionSelected}
+        isAnswered={isAnswered}
+        isAnsweredCorrect={isAnswered && isSelectedCorrect}
+        onClick={handleButtonContinueClick}
+      >
+        Tiếp tục
+      </ButtonContinueWrapper>
+    </div>
+  );
+};
+
+export default QuizBestAnswerWrapper;
--- src/modules/quiz/pages/QuizPage/QuizPage.tsx
+++ src/modules/quiz/pages/QuizPage/QuizPage.tsx
@@ -1,57 +1,25 @@
-import "./QuizPage.css";
+import "./QuizPage.scss";
 
-import React, { useState } from "react";
+import React from "react";
 
-import ButtonContinue from "../../components/ButtonContinue/ButtonContinue";
-import QuizOption from "../../components/QuizOption/QuizOption";
+import QuizConnectWrapper from "../../components/QuizConnectWrapper/QuizConnectWrapper";
+import QuizBestAnswerWrapper from "../../components/QuizBestAnswerWrapper/QuizBestAnswerWrapper";
 
 const QuizPage: React.FC = () => {
-  const [isAnswered, setIsAnswered] = useState<boolean>(false);
-  const [selectedOption, setSelectedOption] = useState<null | number>(null);
-  const [isSelectedCorrect, setIsSelectedCorrect] = useState<boolean>(false);
-  const isOptionSelected = selectedOption !== null;
-  const quizOptions = [
-    { id: 1, description: "1203g", isCorrect: true },
-    { id: 2, description: "1204g", isCorrect: false },
-    { id: 3, description: "1205g", isCorrect: false },
-    { id: 4, description: "1206g", isCorrect: false },
-  ];
+  // COMMON PART
+  //
 
-  const handleSelectOption = (
-    idQuizOption: number,
-    isQuizOptionCorrect: boolean
-  ) => {
-    if (!isAnswered) {
-      setSelectedOption(idQuizOption);
-      setIsSelectedCorrect(isQuizOptionCorrect);
-    }
-  };
+  // SEPERATE PART
+  // Select best
 
-  const handleButtonContinueClick = () => {
-    setIsAnswered(true);
-  };
+  //
 
   return (
     <div className="quiz-page">
-      {quizOptions.map((quiz) => (
-        <QuizOption
-          key={quiz.id}
-          isSelected={quiz.id === selectedOption}
-          isAnswered={isAnswered}
-          isCorrect={quiz.isCorrect}
-          onClick={() => handleSelectOption(quiz.id, quiz.isCorrect)}
-        >
-          {quiz.description}
-        </QuizOption>
-      ))}
-      <ButtonContinue
-        isCompleteAnswer={isOptionSelected}
-        isAnswered={isAnswered}
-        isAnsweredCorrect={isAnswered && isSelectedCorrect}
-        onClick={handleButtonContinueClick}
-      >
-        Tiếp tục
-      </ButtonContinue>
+      <div className="quiz-page__answer">
+        <QuizConnectWrapper />
+        <QuizBestAnswerWrapper />
+      </div>
     </div>
   );
 };
